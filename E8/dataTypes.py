2bcf5bc30ad6d0b3cd6a1f28394cbaa44a94baef
rivan <rivan.pratama@amarbank.co.id>
2024-06-13 12:08:22 +0700

    Update: update python readme

--- E8/dataTypes.py
+++ E8/dataTypes.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
 def main():
-  # string
-  # int
-  # float
-  # bool
-  type(True)  # Case Sensitice
-  type(False) # Case Sensitice
-  # string
-  # string
-  # string
+  type(False) 
 
 # Standard boilerplate to call the main() function to begin
 # the program.
